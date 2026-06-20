48e53878259db479949549a3f87f0e9b47c46843
Pedro <ps997099@gmail.com>
2026-06-20 15:37:06 -0300

    feat: projeto inicial do Projeto Nomade

---
+++ app/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <p>Dra. Maria Alice</p>
+      <nav>
+        <a href="#">Início</a>
+        <a href="#">Trabalhos</a>
+        <a href="#">Contato</a>
+      </nav>
+    </header>
+  );
+}
